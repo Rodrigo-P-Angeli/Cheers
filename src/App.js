21583973108eb0af80fbf38494acbbd74bbd7655
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-07-23 17:50:51 -0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>SADDFG</Text>
+            </View>
+        )
+    }
+}
