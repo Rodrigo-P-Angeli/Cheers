7b0558ebae368a350d048f582da3b1da24069e21
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-07-23 17:57:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>SADDFdasfghG</Text>
+                <Text>dfg</Text>
             </View>
         )
     }
