63b39d10bea7947075e654e78aa5f7eb8338b687
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-07-23 17:54:09 -0300

    Create Header.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable semi */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class App extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>SADDFG</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
