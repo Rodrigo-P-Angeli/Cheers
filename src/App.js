5897396a9c4cb35934b78aa57dac1540aba12e5a
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-07-25 14:55:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable semi */
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import Header from './components/Header';
 import { connect } from 'react-redux'
-import { loadCardapio } from './store/actions/cardapio';
 import ItemCardapio from './components/CardapioItem';
+
+import { loadCardapio } from './store/actions/cardapio';
+import { calcTotalItem } from './store/actions/total';
 
 class App extends Component {
     state = {
@@ -13,7 +15,6 @@ class App extends Component {
     }
     componentDidMount() {
         this.props.onLoadScreen()
-        //database().ref('cardapio').on('value', snapshot => {this.setState({cardapio: snapshot.val()})})
     }
     render() {
         return (
@@ -22,7 +23,27 @@ class App extends Component {
                 <FlatList
                     data={this.props.cardapio}
                     keyExtractor={item => `${item.id}`}
-                    renderItem={({ item }) => <ItemCardapio {...item} />} />
+                    renderItem={({ item }) => <ItemCardapio {...item} setTotal={this.props.onSetItemQt()} />} />
+                <View style={styles.body}>
+                    <View style={styles.bottom}>
+                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+                            <View style={{ flex: 2, justifyContent: 'space-between' }}>
+                                <Text style={styles.texTotal}>Total:</Text>
+                                <Text style={{ fontWeight: 'bold' }}> R$:  </Text>
+                            </View>
+                            <View style={styles.containerTotal}>
+                                <Text style={styles.total}>{this.props.total.toFixed(2).replace('.', ',')}</Text>
+                            </View>
+                        </View>
+                        <View style={{ flex: 1 }}>
+                            <Button
+                                //onPress={this.finalizaPedido}
+                                title={'Finalizar pedido'}
+                                color={Cores.Botoes}
+                            />
+                        </View>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -42,7 +63,9 @@ const mapStateToProps = ({ cardapio, total }) => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onLoadScreen: () => dispatch(loadCardapio())
+        onLoadScreen: () => dispatch(loadCardapio()),
+        onSetItemQt: () => dispatch(calcTotalItem()),
+
     }
 }
 
