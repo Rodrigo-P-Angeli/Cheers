5f8d6f771d2bf95133a1649f114b895097ab9b31
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-07-27 15:36:41 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={() => /*this.props.navigation.openDrawer()*/ { }} style={styles.buttonBars}>
+                <TouchableOpacity onPress={() => this.props.navigation.openDrawer()} style={styles.buttonBars}>
                     <Icon name={'bars'} size={20} color={'white'} />
                 </TouchableOpacity>
                 <Text style={styles.title}>Cheers</Text>
