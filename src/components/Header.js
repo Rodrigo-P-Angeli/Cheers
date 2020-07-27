4167661cc83c9c6b4e7dde5273ac6246a8ff3356
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-07-27 15:28:46 -0300

    Install Dependencies

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable semi */
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <TouchableOpacity onPress={() => /*this.props.navigation.openDrawer()*/ { }} style={styles.buttonBars}>
+                    <Icon name={'bars'} size={20} color={'white'} />
+                </TouchableOpacity>
                 <Text style={styles.title}>Cheers</Text>
             </View>
         )
@@ -17,7 +21,6 @@ const styles = StyleSheet.create({
     container: {
         height: 100,
         backgroundColor: '#841584',
-        justifyContent: 'center',
         flexDirection: 'row',
     },
     title: {
@@ -26,5 +29,10 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         color: 'white',
         fontWeight: 'bold',
+        paddingLeft: Dimensions.get('window').width / 3,
+    },
+    buttonBars: {
+        alignSelf: 'center',
+        padding: 10,
     },
 })
