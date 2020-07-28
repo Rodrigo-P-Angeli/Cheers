fe6e9a77aa8e44c88459ef2937243c40bbb14fed
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-07-27 22:00:06 -0300

    Create SplashScreen.js

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+
+import React, { Component } from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
+
+export default class AuthorApp extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size={'large'} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#333',
+    },
+})
