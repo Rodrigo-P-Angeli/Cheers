f97338d88364bc4af59097045f2c8b06d0e3c46b
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-05 15:12:28 -0300

    Update PedidosRealizados.js

--- src/screens/PedidosRealizados.js
+++ src/screens/PedidosRealizados.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import database from '@react-native-firebase/database';
+import { connect } from 'react-redux';
 
-export default class PedidosRealizados extends Component {
+import Header from '../components/Header'
+
+class PedidosRealizados extends Component {
     state = {
         pedidos: []
     }
 
-    componentDidMount = () => {
-        await database().ref('users').child(`${user.uid}/pedidos`).once('value').then(snapshot => {
-            let pedidos = snapshot.val() ? snapshot.val() : []
-            pedidos.map(element => {
-                await database().ref('pedidos').child(`${element}`).once('value').then(snapshot => {
-                    let pedidos2 = snapshot.val()
-                    this.setState({ pedidos: pedidos2 })
-                })
-            });
-        })
+    componentDidMount = async () => {
+        let pedidos2 = []
+        try {
+            await database().ref('users').child(`${this.props.user.uid}/pedidos`).once('value').then(snapshot => {
+                let pedidos = snapshot.val() ? snapshot.val() : []
+                pedidos.forEach(element => {
+                    database().ref('pedidos').child(`${element}`).once('value').then(snapshot => {
+                        pedidos2.push(snapshot.val())
+                        this.setState({ pedidos: pedidos2 })
+                    })
+                    console.log(pedidos2)
+                });
+                console.log(pedidos2)
+            })
+            console.log(pedidos2)
+        }
+        catch (err) {
+            console.log(err)
+        }
+
     }
 
     render() {
         return (
             <View style={styles.container}>
-                {this.state.pedidos.map(item => <Text>{item.marca}</Text>)}
+                <Header {...this.props} />
+                {console.log(this.state.pedidos)}
+                {this.state.pedidos.map((item) => item.pedido.map(item =>
+                    <Text>
+                        {item.marca}  {item.quantidade}  {item.price}
+                    </Text>))}
             </View>
         )
     }
@@ -32,3 +51,11 @@ const styles = StyleSheet.create({
         flex: 1,
     },
 })
+
+const mapStateToProps = ({ user }) => {
+    return {
+        user: user.user,
+    }
+}
+
+export default connect(mapStateToProps)(PedidosRealizados)
