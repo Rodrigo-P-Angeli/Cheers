6b805d1be8887a8280fa9b43b484c9fb80d27c7c
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-05 18:31:27 -0300

    Update cardapio.js

--- src/store/actions/cardapio.js
+++ src/store/actions/cardapio.js
@@ -108,7 +108,6 @@ export const pushPedido = () => {
 
 export const postPedido = (user, pedido, endereco, total) => {
     let refPedido = null
-    console.log(total)
     return async dispatch => {
         try {
             refPedido = await database().ref('pedidos').push()
