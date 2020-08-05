ac71173b1b0f9727b73dabf1bdac5f44a2f07c70
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-05 18:26:00 -0300

    Update User.js

--- src/store/reducers/User.js
+++ src/store/reducers/User.js
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable semi */
 
-import { USER_LOGGING, USER_LOGOUT } from '../ActionsTypes'
+import { USER_LOGGING, USER_LOGOUT, USER_SET_CEP, USER_SET_NUMERO, USER_SET_CIDADE, USER_SET_COMPLEMENTO, USER_SET_BAIRRO, USER_SET_RUA } from '../ActionsTypes'
 
 const initialState = {
     endereco: {
+        CEP: '',
         estado: 'ES',
         cidade: '',
         bairro: '',
@@ -27,6 +28,54 @@ const reducer = (state = initialState, action) => {
         case USER_LOGOUT:
             return {
                 ...initialState,
+            }
+        case USER_SET_CEP:
+            return {
+                ...state,
+                endereco: {
+                    ...state.endereco,
+                    CEP: action.payload.text
+                }
+            }
+        case USER_SET_CIDADE:
+            return {
+                ...state,
+                endereco: {
+                    ...state.endereco,
+                    cidade: action.payload.text
+                }
+            }
+            case USER_SET_NUMERO:
+            return {
+                ...state,
+                endereco: {
+                    ...state.endereco,
+                    numero: action.payload.text
+                }
+            }
+            case USER_SET_COMPLEMENTO:
+            return {
+                ...state,
+                endereco: {
+                    ...state.endereco,
+                    complemento: action.payload.text
+                }
+            }
+            case USER_SET_BAIRRO:
+            return {
+                ...state,
+                endereco: {
+                    ...state.endereco,
+                    bairro: action.payload.text
+                }
+            }
+            case USER_SET_RUA:
+            return {
+                ...state,
+                endereco: {
+                    ...state.endereco,
+                    rua: action.payload.text
+                }
             }
         default:
             return state
