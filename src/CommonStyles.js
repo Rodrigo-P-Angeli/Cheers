a44d1dd06c3163930a86d2c680b53310322e8f56
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-07 17:28:05 -0300

    Update CommonStyles.js

--- src/CommonStyles.js
+++ src/CommonStyles.js
@@ -1,9 +1,11 @@
 export default {
     fontFamily: 'Roboto-Light',
+    fontFamilyTitle: 'Roboto-Bold',
     Colors: {
         secundary: '#FFF',
         mainText: '#222',
-        subText:'#555'
+        subText:'#555',
+        buttons: '#841584'
     }
 
 }
