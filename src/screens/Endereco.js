6eb47e8f4f1fa4e9ecc9e9e7cc01cf72421ec8d8
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-12 15:04:47 -0300

    Update Endereco.js

--- src/screens/Endereco.js
+++ src/screens/Endereco.js
@@ -13,6 +13,15 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 import { onChangeCEP, onChangeCidade, onChangeBairro, onChangeComplemento, onChangeNumero, onChangeRua } from '../store/actions/user'
 import CommonStyles from '../CommonStyles';
 
+const homePlace = {
+    description: 'Home',
+    geometry: { location: { lat: 48.8152937, lng: 2.4597668 } },
+};
+const workPlace = {
+    description: 'Work',
+    geometry: { location: { lat: 48.8496818, lng: 2.2940881 } },
+};
+
 class Endereco extends Component {
     state = {
         endereco: {
@@ -26,20 +35,62 @@ class Endereco extends Component {
     }
     render() {
         return (
-            <ImageBackground style={styles.container} source={require('../../assets/images/BackGroundBody.jpg')}>
+            <View style={styles.container}>
+                {/* <ImageBackground style={styles.container} source={require('../../assets/images/BackGroundBody.jpg')}> */}
                 <Header {...this.props} />
                 <GooglePlacesAutocomplete
                     placeholder='Search'
+                    minLength={2} // minimum length of text to search
+                    autoFocus={false}
+                    fetchDetails={true}
                     onPress={(data, details = null) => {
                         // 'details' is provided when fetchDetails = true
-                        console.log(data, details);
+                        console.log(data);
+                        console.log(details);
+                    }}
+                    getDefaultValue={() => {
+                        return ''; // text input default value
                     }}
                     query={{
-                        key: 'AIzaSyByMXFjuIh-iA6BQQnv1d4PiqzDTqQkKD0',
-                        language: 'en',
+                        // available options: https://developers.google.com/places/web-service/autocomplete
+                        key: 'YOUR API KEY',
+                        language: 'en', // language of the results
+                        types: '(cities)', // default: 'geocode'
                     }}
+                    styles={{
+                        description: {
+                            fontWeight: 'bold',
+                        },
+                        predefinedPlacesDescription: {
+                            color: '#1faadb',
+                        },
+                    }}
+                    currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+                    currentLocationLabel='Current location'
+                    nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                    GoogleReverseGeocodingQuery={
+                        {
+                            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+                        }
+                    }
+                    GooglePlacesSearchQuery={{
+                        // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                        rankby: 'distance',
+                        types: 'food',
+                    }}
+                    GooglePlacesDetailsQuery={{
+                        // available options for GooglePlacesDetails API : https://developers.google.com/places/web-service/details
+                        fields: 'formatted_address',
+                    }}
+                    filterReverseGeocodingByTypes={[
+                        'locality',
+                        'administrative_area_level_3',
+                    ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+                    predefinedPlaces={[homePlace, workPlace]}
+                    predefinedPlacesAlwaysVisible={true}
                 />
-            </ImageBackground>
+                {/* </ImageBackground> */}
+            </View>
         );
     }
 }
