e89951cf7bde231221da2c17269f164775011220
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-12 16:36:57 -0300

    Update PedidosRealizados.js

--- src/screens/PedidosRealizados.js
+++ src/screens/PedidosRealizados.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Text, ScrollView } from 'react-native'
+import { View, StyleSheet, Text, ScrollView, ImageBackground } from 'react-native'
 import database from '@react-native-firebase/database';
 import { connect } from 'react-redux';
 
 import Header from '../components/Header'
 import ItemPedido from '../components/ItemPedido'
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import CommonStyles from '../CommonStyles';
 
 const initialState = {
     pedidos: []
@@ -38,17 +39,27 @@ class PedidosRealizados extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
+            <ImageBackground style={styles.container} source={require('../../assets/images/BackGroundBody.jpg')}>
                 <Header {...this.props} />
                 <ScrollView>
                     {this.state.pedidos.map((item) =>
-                        <TouchableOpacity key={Math.random()}>
-                            <Text>{JSON.stringify(item.endereco)}</Text>
-                            <Text>{JSON.stringify(item.total)}</Text>
-                            {item.pedido.map(item => <ItemPedido {...item} key={Math.random()} />)}
-                        </TouchableOpacity>)}
+                        <View style={styles.item} key={Math.random()}>
+                            <View style={{ height: 100, flex: 2, justifyContent: 'space-between', borderWidth: 1 }}>
+                                <Text style={styles.text}>Data: {item.data}</Text>
+                                <Text style={styles.text}>Entrega em: {item.endereco.rua}, n° {item.endereco.numero}, {item.endereco.bairro}, {item.endereco.cidade}, {item.endereco.estado}{item.endereco.complemento ? `, ${item.endereco.complemento}` : null}</Text>
+                                <Text style={styles.text}>Status: {item.status}</Text>
+                            </View>
+                            <View style={{ height: 100, flex: 1, justifyContent: 'space-between', borderWidth: 1 }}>
+                                <Text style={styles.text}># {item.numeroPedido}</Text>
+                                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                    <Text style={[styles.text, { fontSize: 15 }]}>{'Total:\nR$'}</Text>
+                                    <Text style={[styles.text, { flex: 1, textAlign: 'right', fontSize: 20 }]}>{item.total.toFixed(2).replace('.', ',')}</Text>
+                                </View>
+                            </View>
+                            {/* {item.pedido.map(item => <ItemPedido {...item} key={Math.random()} />)} */}
+                        </View>)}
                 </ScrollView>
-            </View>
+            </ImageBackground>
         )
     }
 }
@@ -56,7 +67,25 @@ class PedidosRealizados extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        borderWidth: 1,
     },
+    item: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderColor: '#e3e3e3',
+        padding: 5,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        margin: 5,
+        borderWidth: 1,
+        height: 100,
+    },
+    text: {
+        fontFamily: CommonStyles.fontFamily,
+        fontSize: 15,
+        //padding: 2,
+    }
 })
 
 const mapStateToProps = ({ user }) => {
