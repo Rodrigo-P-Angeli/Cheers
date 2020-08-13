b69a0bc79546b8d345294500051956f0dc4841fc
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-13 16:32:07 -0300

    Update PedidosRealizados.js

--- src/screens/PedidosRealizados.js
+++ src/screens/PedidosRealizados.js
@@ -49,20 +49,22 @@ class PedidosRealizados extends Component {
                 <Header {...this.props} />
                 {this.state.pedidos.length ? <ScrollView>
                     {this.state.pedidos.map((item) =>
-                        <View style={styles.item} key={item.numeroPedido}>
-                            <View style={{ height: 100, flex: 2, justifyContent: 'space-between' }}>
-                                <Text style={styles.text}>Data: {Moment(item.data).format('DD/MM/YYYY h:mm:ss a')}</Text>
-                                <Text style={styles.text}>Entrega em: {item.endereco.rua}, n° {item.endereco.numero}, {item.endereco.bairro}, {item.endereco.cidade}, {item.endereco.estado}{item.endereco.complemento ? `, ${item.endereco.complemento}` : null}</Text>
-                                <Text style={styles.text}>Status: {item.status}</Text>
-                            </View>
-                            <View style={{ height: 100, flex: 1, justifyContent: 'space-between' }}>
-                                <Text style={styles.text}># {item.numeroPedido}</Text>
-                                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                    <Text style={[styles.text, { fontSize: 15 }]}>{'Total:\nR$'}</Text>
-                                    <Text style={[styles.text, { flex: 1, textAlign: 'right', fontSize: 20 }]}>{item.total.toFixed(2).replace('.', ',')}</Text>
+                        <View key={item.numeroPedido} style={{ backgroundColor: 'white', margin: 5, padding: 5, borderRadius: 10 }} elevation={5}>
+                            <View style={styles.item}>
+                                <View style={{ height: 100, flex: 2, justifyContent: 'space-between' }}>
+                                    <Text style={styles.text}>Data: {Moment(item.data).format('DD/MM/YYYY h:mm:ss a')}</Text>
+                                    <Text style={styles.text}>Entrega em: {item.endereco.rua}, n° {item.endereco.numero}, {item.endereco.bairro}, {item.endereco.cidade}, {item.endereco.estado}{item.endereco.complemento ? `, ${item.endereco.complemento}` : null}</Text>
+                                    <Text style={styles.text}>Status: {item.status}</Text>
                                 </View>
+                                <View style={{ height: 100, flex: 1, justifyContent: 'space-between' }}>
+                                    <Text style={styles.text}># {item.numeroPedido}</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                        <Text style={[styles.text, { fontSize: 15 }]}>{'Total:\nR$'}</Text>
+                                        <Text style={[styles.text, { flex: 1, textAlign: 'right', fontSize: 20 }]}>{item.total.toFixed(2).replace('.', ',')}</Text>
+                                    </View>
+                                </View>
+                                {/* {item.pedido.map(item => <ItemPedido {...item} key={Math.random()} />)} */}
                             </View>
-                            {/* {item.pedido.map(item => <ItemPedido {...item} key={Math.random()} />)} */}
                         </View>)
                     }
                 </ScrollView> :
@@ -89,11 +91,10 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        borderColor: '#e3e3e3',
+        borderColor: CommonStyles.Colors.buttons,
         padding: 5,
-        borderRadius: 10,
+        borderRadius: 5,
         backgroundColor: 'white',
-        margin: 5,
         borderWidth: 1,
         height: 100,
     },
