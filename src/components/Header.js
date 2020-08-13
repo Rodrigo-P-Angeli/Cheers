82b37a522f2fdf14ef13647e8bfc084064a8d72d
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-13 15:21:38 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        height: 100,
+        height: 80,
         flexDirection: 'row',
         resizeMode: 'contain',
     },
