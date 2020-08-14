31d829de52f0d4866240fecb88191a83834fbb44
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-14 16:41:43 -0300

    Update ConfirmaPedido.js

--- src/screens/ConfirmaPedido.js
+++ src/screens/ConfirmaPedido.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Text, ScrollView, ImageBackground } from 'react-native'
+import { View, StyleSheet, Text, ScrollView, ImageBackground, FlatList } from 'react-native'
 import database from '@react-native-firebase/database';
 import { connect } from 'react-redux';
 
@@ -10,6 +10,8 @@ import { postPedido } from '../store/actions/cardapio';
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import Feather from 'react-native-vector-icons/Feather'
 
+import ItemPedido from '../components/ItemPedido'
+
 class ConfirmaPedido extends Component {
 
     render() {
@@ -18,17 +20,32 @@ class ConfirmaPedido extends Component {
                 <View>
                     <Header hidden={true} {...this.props} />
                     <View style={styles.container}>
-
+                        <View>
+                            <Text style={styles.title}>Finalizar pedido</Text>
+                            <Text style={styles.subtitle}>Favor conferir todos os dados se estão corretos</Text>
+                        </View>
+                        <View style={styles.enderecos}>
+                            <Text style={styles.text}>Entrega em: {this.props.endereco.rua}, n° {this.props.endereco.numero}, {this.props.endereco.bairro}, {this.props.endereco.cidade}, {this.props.endereco.estado}{this.props.endereco.complemento ? `, ${this.props.endereco.complemento}` : null}</Text>
+                        </View>
+                        <FlatList
+                            style={styles.flatlist}
+                            data={this.props.cardapio}
+                            keyExtractor={item => `${item.id}`}
+                            renderItem={({ item }) => item.quantidade > 0 ?
+                                <ItemPedido {...item} />
+                                : null}
+                        />
+                        <View style={styles.totalcontainer}><Text style={styles.Total}>R$ {this.props.total.toFixed(2).replace('.', ',')}</Text></View>
                     </View>
                     <View style={styles.buttons}>
-                        <TouchableOpacity
+                        {/* <TouchableOpacity
                             activeOpacity={.6}
                             style={[styles.finalizar, { backgroundColor: 'red',  }]}
                             onPress={() => {
                                 this.props.navigation.navigate('AppDrawer')
                             }}>
                             <Feather name={'x'} size={50} color={'white'} />
-                        </TouchableOpacity>
+                        </TouchableOpacity> */}
                         <TouchableOpacity
                             activeOpacity={.6}
                             style={styles.finalizar}
@@ -51,12 +68,11 @@ const styles = StyleSheet.create({
         resizeMode: 'contain'
     },
     container: {
-        justifyContent: 'center',
         margin: 30,
-        backgroundColor: 'white',
-        width: '80%',
-        height: '60%',
-        alignSelf: 'center'
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        width: '90%',
+        alignSelf: 'center',
+        padding: 10,
     },
     finalizar: {
         height: 80,
@@ -65,10 +81,45 @@ const styles = StyleSheet.create({
         backgroundColor: 'green',
         alignItems: 'center',
         justifyContent: 'center',
+        margin: 20,
     },
     buttons: {
         flexDirection: 'row',
         justifyContent: 'space-around'
+    },
+    title: {
+        fontSize: 30,
+        textAlign: 'center',
+        color: 'white',
+        fontFamily: CommonStyles.fontFamilyTitle
+    },
+    subtitle: {
+        textAlign: 'center',
+        marginBottom: 10,
+        color: 'white',
+        fontFamily: CommonStyles.fontFamily
+    },
+    text: {
+        fontSize: 15,
+        color: 'white',
+        fontFamily: CommonStyles.fontFamily
+    },
+    totalcontainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    Total: {
+        marginTop: 10,
+        borderColor: 'white',
+        borderTopWidth: 1,
+        fontSize: 20,
+        textAlign: 'right',
+        flex: .4,
+        color: 'white',
+        fontFamily: CommonStyles.fontFamily
+    },
+    flatlist: {
+        marginTop: 30,
     }
 })
 
