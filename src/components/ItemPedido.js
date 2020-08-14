a6380bcdabeb06feddf3c4eb51364fb33a9ba912
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-08-14 16:41:41 -0300

    Update ItemPedido.js

--- src/components/ItemPedido.js
+++ src/components/ItemPedido.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import CommonStyles from '../CommonStyles'
 
 export default class ListaItems extends Component {
-    render () {
-        return(
+    render() {
+        return (
             <View style={styles.container}>
-                <Text>{this.props.marca}</Text>
-                <Text>{this.props.tipo}</Text>
-                <Text>{this.props.quantidade}</Text>
-                <Text>{this.props.total}</Text>
-                <Text>{this.props.price}</Text>
+                <View style={{ flex: 2 }}>
+                    <Text style={styles.desc}>{this.props.marca} {this.props.tipo}</Text>
+                    <Text style={styles.price}>{this.props.price.toFixed(2).replace('.', ',')}</Text>
+                </View>
+                <Text style={styles.desc}> x{this.props.quantidade}</Text>
+                <Ionicons name={'arrow-forward-sharp'} color={'white'} size={10} style={styles.desc} />
+                <Text style={styles.total}>{this.props.total.toFixed(2).replace('.', ',')}</Text>
             </View>
         )
     }
@@ -18,5 +22,21 @@ export default class ListaItems extends Component {
 const styles = StyleSheet.create({
     container: {
         paddingTop: 5,
+        flexDirection: 'row',
     },
+    desc: {
+        flex: 1,
+        color: 'white',
+        fontFamily: CommonStyles.fontFamily
+    },
+    price: {
+        color: 'white',
+        fontFamily: CommonStyles.fontFamily,
+    },
+    total: {
+        flex: 1,
+        color: 'white',
+        fontFamily: CommonStyles.fontFamily,
+        textAlign: 'right'
+    }
 })
