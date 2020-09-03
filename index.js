f9e4d624a0f1433f20386535e807dc2a663045aa
Rodrigo-P-De-Angeli <Rodrigo-P-De-Angeli@users.noreply.github.com>
2020-09-03 18:00:56 -0300

    Configurando...

--- index.js
+++ index.js
@@ -10,15 +10,28 @@ import { name as appName } from './app.json';
 
 import App from './src/Navigation';
 import { Provider as StoreProvider } from 'react-redux';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import storeConfig from './src/store/StoreConfig'
 
 const store = storeConfig()
-
+const fontConfig = {
+    default: {
+        regular: {
+            fontFamily: 'Roboto-Medium',
+        },
+        light: {
+            fontFamily: 'Roboto-Light',
+        },
+        bold: {
+            fontFamily: 'Roboto-Bold',
+        },
+    },
+};
 const theme = {
     ...DefaultTheme,
     roundness: 2,
     dark: true,
+    fonts: configureFonts(fontConfig),
 };
 
 const Redux = () => {
